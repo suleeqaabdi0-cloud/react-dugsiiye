c51deb44713d207349584aa366450a518fbcc66d
osmoha5 <osmanmoha564@gmail.com>
2026-06-20 20:45:13 +0300

    exercise-6

---
+++ src/exercise-6/App.jsx
@@ -0,0 +1,11 @@
+import GreetingTitle from './GreetingTitle';
+
+const App = () => {
+  return (
+    <div>
+      <GreetingTitle />
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './exercise-5/App.jsx'
+import App from './exercise-6/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
