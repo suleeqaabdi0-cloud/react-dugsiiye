f28c60fc7b95f8a64f059dccf137289cbd413185
osmoha5 <osmanmoha564@gmail.com>
2026-06-28 20:58:27 +0300

    exercise-12

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './exercise-11/App.jsx'
+import App from './exercise-12/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
