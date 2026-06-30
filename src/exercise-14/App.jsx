5a06a6c70fad40d42d2acc36a0bed2759caafb7d
osmoha5 <osmanmoha564@gmail.com>
2026-06-30 22:51:50 +0300

    exercise-14

---
+++ src/exercise-14/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MultiStepForm from './MultiStepForm';
+
+function App() {
+  return (
+    <div>
+      <MultiStepForm />
+    </div>
+  );
+}
+
+export default App;
